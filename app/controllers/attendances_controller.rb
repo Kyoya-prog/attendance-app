32b6ea04d2f2cb4254ed367eacca65499e21eb64
Kyoya-prog <kyoyaprog@gmail.com>
2021-12-24 16:02:55 +0900

    処理を見直し

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,13 +1,12 @@
 class AttendancesController < ApplicationController
   def new
-
+    @record = current_user.attendances.last
   end
   def index
 
   end
 
   def register_work_in
-    @record = current_user.attendances.last
     if !@record || @record.work_out != nil
       new_record = current_user.attendances.build
       new_record.work_in = current_date_time
@@ -23,21 +22,21 @@ class AttendancesController < ApplicationController
   end
 
   def register_break_in
-    if @record.work_in != nil
+    if @record || @record.work_in != nil
       @record.break_in = current_date_time
       @record.update
     end
   end
 
   def register_break_out
-    if @record.work_in != nil && @record.break_in != nil
+    if @record || @record.work_in != nil && @record.break_in != nil
       @record.break_out = current_date_time
       @record.update
     end
   end
 
   def register_work_out
-    if @record.work_in != nil
+    if @record || @record.work_in != nil
       @record.work_out = current_date_time
       @record.update
     end
