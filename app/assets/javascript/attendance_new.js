64a3638e97ad14ca1e1d5f54ba51c93e899369b5
kyoya <kyoyaprog@gmail.com>
2022-01-10 13:39:04 +0900

    CSSを修正

--- app/assets/javascript/attendance_new.js
+++ app/assets/javascript/attendance_new.js
@@ -3,12 +3,8 @@ const item = document.getElementById('clock');
 
 function time() {
     let today = new Date();
-    console.log(today.toLocaleString("ja"));
-    console.log(item)
     item.innerHTML = today.toLocaleString("ja");
     window.requestAnimationFrame(time);
 };
-
-console.log("hogehoge")
 
 time();
