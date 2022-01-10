6401c2a345f23dadcd3fdfb921b91a14d27e753d
kyoya <kyoyaprog@gmail.com>
2022-01-10 13:50:41 +0900

    javascriptのコードをjsファイルにまとめる

--- app/assets/javascript/attendance_new.js
+++ app/assets/javascript/attendance_new.js
@@ -1,5 +1,7 @@
 const item = document.getElementById('clock');
 
+const date = document.getElementById("current_date")
+
 
 function time() {
     let today = new Date();
@@ -7,4 +9,15 @@ function time() {
     window.requestAnimationFrame(time);
 };
 
+function setDate() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate()
+    date.innerHTML = `今日は${month}月${day}日です`;
+}
+
 time();
+
+setDate();
+
