e6fb9d65f831a055d1eb5d092d9d8cc58fd3e84c
Kyoya-prog <kyoyaprog@gmail.com>
2022-01-11 14:03:50 +0900

    集計処理をviewmodelにやらせる

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -4,10 +4,18 @@ class Attendance < ApplicationRecord
   validates :user_id, presence: true
 
   def restraint_time
-    work_out - work_in
+    if work_out
+      work_out - work_in
+    else
+      0
+    end
   end
 
   def work_time
-    work_out - work_in - break_time
+    if work_out
+      work_out - work_in - break_time
+    else
+      0
+    end
   end
 end
